e5e5a594bafb91e8f36a4f5ff56190602dc3ce1e
yuraslivka <slivka.y@gmail.com>
2021-10-06 23:38:46 +0200

    add 06 2338

--- js/common.js
+++ js/common.js
@@ -1,3 +1,12 @@
+let toggle = document.querySelector('.toggle');
+let navigation = document.querySelector('.navigation');
+let main = document.querySelector('.main');
+
+toggle.onclick = function () {
+    navigation.classList.toggle('active');
+    main.classList.toggle('active');
+}
+
 let list = document.querySelectorAll('.navigation li');
 function activeLink() {
     list.forEach((item) =>
